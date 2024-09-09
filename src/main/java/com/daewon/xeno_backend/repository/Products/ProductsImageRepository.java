eeff2964b883f7cd565fb09f421ac6c86bb72fb0
joohyeongzz <162415872+joohyeongzz@users.noreply.github.com>
2024-09-09 09:43:00 +0900

    도메인 변경

--- src/main/java/com/daewon/xeno_backend/repository/Products/ProductsImageRepository.java
+++ src/main/java/com/daewon/xeno_backend/repository/Products/ProductsImageRepository.java
@@ -28,17 +28,17 @@ public interface ProductsImageRepository extends JpaRepository<ProductsImage, Lo
     // 또는 여러 이미지 ID로 조회
     List<ProductsImage> findByProductImageIdIn(List<Long> productImageIds);
 
-    @Query("SELECT u FROM ProductsImage u WHERE u.productNumber = :productNumber AND u.users.brand = :brand AND u.products IS NOT NULL")
+    @Query("SELECT u FROM ProductsImage u WHERE u.productNumber = :productNumber AND u.brand = :brand AND u.products IS NOT NULL")
     ProductsImage findByProductNumberAndUsersAndProductsIsNotNull(String productNumber, Brand brand);
 
-    @Query("SELECT u FROM ProductsImage u WHERE u.productNumber = :productNumber AND u.users = :users")
-    ProductsImage findByProductNumberAndUsers(String productNumber, Users users);
+    @Query("SELECT u FROM ProductsImage u WHERE u.productNumber = :productNumber AND u.brand = :brand")
+    ProductsImage findByProductNumberAndUsers(String productNumber, Brand brand);
 
-    @Query("SELECT u FROM ProductsImage u WHERE u.users = :users")
-    List<ProductsImage> findByUsers(Users users);
+    @Query("SELECT u FROM ProductsImage u WHERE u.brand = :brand")
+    List<ProductsImage> findByUsers(Brand brand);
 
-    @Query("SELECT p FROM ProductsImage p WHERE p.products IS NULL AND p.users = :users")
-    List<ProductsImage> findByProductsIsNullAndUsers(Users users);
+    @Query("SELECT p FROM ProductsImage p WHERE p.products IS NULL AND p.brand = :brand")
+    List<ProductsImage> findByProductsIsNullAndUsers(Brand brand);
 
     @Query("SELECT u FROM ProductsImage u WHERE u.products IS NULL")
     List<ProductsImage> findImagesWithoutProductId();
